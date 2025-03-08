dc4868089cbebf709630ce8e5d0a0a68644364a7
Sigma-dev <antonin.duthil@epitech.eu>
2025-03-08 22:13:01 +0100

    (tempo): added steamworks messages example

--- src/main.rs
+++ src/main.rs
@@ -1,254 +1,82 @@
-use bevy::prelude::*;
-use crossbeam_channel::*;
-use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+use macroquad::prelude::*;
 use steamworks::{
-    networking_types::{NetConnectionEnd, NetworkingIdentity, NetworkingMessage, SendFlags},
-    *,
+    networking_types::{NetworkingIdentity, SendFlags},
+    FriendFlags,
 };
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .insert_resource(SteamClient::new())
-        .add_systems(Startup, setup)
-        .add_systems(Update, (handle_receivers, update))
-        .add_systems(PreUpdate, receive_messages)
-        .run();
-}
+#[macroquad::main("steamworks-rs")]
+async fn main() {
+    // 480 is Spacewar!, the Steamworks SDK example app.
+    let client =
+        steamworks::Client::init_app(480).expect("Steam is not running or has not been detected");
 
-#[derive(Resource)]
-struct SteamClient {
-    client: Client,
-    lobby_id: Option<LobbyId>,
-    channel: SteamChannel,
-}
+    // Get the API interfaces
+    let friends = client.friends();
+    let messages = client.networking_messages();
 
-#[derive(Serialize, Deserialize, Debug)]
-struct NetworkData {
-    data: Vec<u8>,
-}
+    // Even though NetworkingMessages appears as ad-hoc API, it's internally session based. We must accept any incoming
+    // messages before communicating with the peer.
+    messages.session_request_callback(move |req| {
+        println!("Accepting session request from {:?}", req.remote());
+        assert!(req.accept());
+    });
 
-impl SteamClient {
-    pub fn new() -> SteamClient {
-        SteamClient {
-            client: Client::init_app(480).unwrap(),
-            lobby_id: None,
-            channel: SteamChannel::new(),
+    // Install a callback to debug print failed peer connections
+    messages.session_failed_callback(|info| {
+        eprintln!("Session failed: {info:#?}");
+    });
+
+    // Keep track of all players
+    let mut peers = HashMap::new();
+
+    loop {
+        // Poll the internal callbacks
+        client.run_callbacks();
+
+        clear_background(BLACK);
+
+        set_camera(&Camera2D::from_display_rect(Rect::new(
+            -1.0, 1.0, 2.0, -2.0,
+        )));
+
+        // Draw us at our mouse position
+        let me = mouse_position_local();
+        draw_circle(me.x, me.y, 0.1, GREEN);
+
+        // Send our mouse position to all friends
+        for friend in friends.get_friends(FriendFlags::IMMEDIATE) {
+            let identity = NetworkingIdentity::new_steam_id(friend.id());
+
+            // Convert our position to bytes
+            let mut data = [0; 8];
+            data[0..4].copy_from_slice(&me.x.to_le_bytes());
+            data[4..8].copy_from_slice(&me.y.to_le_bytes());
+
+            let _ =
+                messages.send_message_to_user(identity, SendFlags::UNRELIABLE_NO_DELAY, &data, 0);
         }
-    }
 
-    pub fn steam_id(&self) -> SteamId {
-        self.client.user().steam_id()
-    }
+        // Receive messages from the network
+        for message in messages.receive_messages_on_channel(0, 100) {
+            let peer = message.identity_peer();
+            let data = message.data();
 
-    pub fn is_in_lobby(&self) -> bool {
-        self.lobby_id.is_some()
-    }
+            // Convert peer position from bytes
+            let peer_x =
+                f32::from_le_bytes(data[0..4].try_into().expect("Someone sent bad message"));
+            let peer_y =
+                f32::from_le_bytes(data[4..8].try_into().expect("Someone sent bad message"));
 
-    pub fn join_lobby(&self, lobby_id: LobbyId) {
-        let tx = self.channel.sender.clone();
-        self.client.matchmaking().join_lobby(lobby_id, move |res| {
-            if let Ok(lobby_id) = res {
-                match tx.send(ChannelMessage::LobbyJoined(lobby_id)) {
-                    Ok(_) => {}
-                    Err(_) => {}
-                }
-            }
-        });
-    }
-
-    pub fn get_players_in_lobby(&self) -> Vec<SteamId> {
-        let Some(lobby_id) = self.lobby_id else {
-            panic!("Not currently in a lobby");
-        };
-        self.client.matchmaking().lobby_members(lobby_id)
-    }
-
-    pub fn send_message_others(&self, data: NetworkData, flags: SendFlags) -> Result<(), String> {
-        if !self.is_in_lobby() {
-            return Err("Not currently in a lobby".to_owned());
-        };
-        for player in self.get_players_in_lobby() {
-            if player == self.steam_id() {
-                continue;
-            }
-            self.send_message(&data, player, flags)
-                .expect("Couldn't send message in send others");
+            peers.insert(peer.debug_string(), (peer_x, peer_y));
         }
-        return Ok(());
-    }
 
-    pub fn send_message(
-        &self,
-        data: &NetworkData,
-        target: SteamId,
-        flags: SendFlags,
-    ) -> Result<(), String> {
-        if !self.is_in_lobby() {
-            return Err("Not in a lobby".to_string());
-        };
-        let serialize_data = rmp_serde::to_vec(&data);
-        let serialized = serialize_data.map_err(|err| err.to_string())?;
-        let data_arr = serialized.as_slice();
-        let network_identity = NetworkingIdentity::new_steam_id(target);
-        let res = self.client.networking_messages().send_message_to_user(
-            network_identity,
-            flags,
-            data_arr,
-            0,
-        );
-        println!("Sent message: {:?} {:?}", data, res);
-        return res.map_err(|e: SteamError| e.to_string());
-    }
-}
-
-enum ChannelMessage {
-    LobbyCreated(LobbyId),
-    LobbyJoinRequest(LobbyId),
-    LobbyJoined(LobbyId),
-    LobbyChatMessage(LobbyChatMsg),
-    LobbyChatUpdate(LobbyChatUpdate),
-}
-
-struct SteamChannel {
-    sender: Sender<ChannelMessage>,
-    receiver: Receiver<ChannelMessage>,
-}
-
-impl SteamChannel {
-    pub fn new() -> SteamChannel {
-        let (sender, receiver) = unbounded();
-        SteamChannel { sender, receiver }
-    }
-}
-
-fn setup(client: Res<SteamClient>) {
-    let sender = client.channel.sender.clone();
-    let tx = client.channel.sender.clone();
-    let tx2 = client.channel.sender.clone();
-
-    client
-        .client
-        .register_callback(move |message: LobbyChatMsg| {
-            println!("Lobby chat message received: {:?}", message);
-            sender
-                .send(ChannelMessage::LobbyChatMessage(message))
-                .unwrap();
-        });
-
-    client
-        .client
-        .register_callback(move |message: LobbyChatUpdate| {
-            println!("Lobby update  received: {:?}", message);
-            tx2.clone()
-                .send(ChannelMessage::LobbyChatUpdate(message))
-                .unwrap();
-        });
-
-    client
-        .client
-        .register_callback(move |message: GameLobbyJoinRequested| {
-            let _ = tx.send(ChannelMessage::LobbyJoinRequest(message.lobby_steam_id));
-        });
-
-    client
-        .client
-        .networking_messages()
-        .session_request_callback(move |session_request| {
-            println!("Received session request");
-            session_request.accept();
-        });
-
-    client
-        .client
-        .networking_messages()
-        .session_failed_callback(move |res| {
-            println!(
-                "Session Failed: {:?}",
-                res.end_reason().unwrap_or(NetConnectionEnd::Other(-42))
-            );
-        });
-}
-
-fn receive_messages(client: Res<SteamClient>) {
-    let messages: Vec<NetworkingMessage<ClientManager>> = client
-        .client
-        .networking_messages()
-        .receive_messages_on_channel(0, 2048);
-
-    for message in messages {
-        println!("Received message: {:?}", message.data());
-        let sender = message.identity_peer().steam_id().unwrap();
-        let serialized_data = message.data();
-        let data_try: Result<NetworkData, _> = rmp_serde::from_slice(serialized_data);
-
-        if let Ok(data) = data_try {
-            println!("Decoded: {:?}", data);
+        // Draw all peers
+        for peer in peers.values() {
+            draw_circle(peer.0, peer.1, 0.1, RED);
         }
-        drop(message); //not sure about usefullness, mentionned in steam docs as release
-    }
-}
 
-fn handle_receivers(mut steam_client: ResMut<SteamClient>) {
-    let tx: Sender<ChannelMessage> = steam_client.channel.sender.clone();
-    if let Ok(msg) = steam_client.channel.receiver.try_recv() {
-        match msg {
-            ChannelMessage::LobbyCreated(lobby_id) => {
-                steam_client.lobby_id = Some(lobby_id);
-                info!("Created lobby {:?}", lobby_id);
-            }
-            ChannelMessage::LobbyJoined(lobby_id) => {
-                steam_client.lobby_id = Some(lobby_id);
-                info!("Joined lobby {:?}", lobby_id)
-            }
-            ChannelMessage::LobbyChatMessage(message) => {
-                let mut buffer = vec![0; 256];
-                let buffer = steam_client.client.matchmaking().get_lobby_chat_entry(
-                    message.lobby,
-                    message.chat_id,
-                    buffer.as_mut_slice(),
-                );
-                info!("Message buffer: [{:?}]", buffer);
-            }
-            ChannelMessage::LobbyChatUpdate(update) => {
-                info!("Update: {:?}", update);
-            }
-            ChannelMessage::LobbyJoinRequest(lobby_id) => {
-                info!("Requested to join lobby {:?}", lobby_id);
-                steam_client.join_lobby(lobby_id);
-            }
-        };
-    }
-}
-
-fn update(keys: Res<ButtonInput<KeyCode>>, steam_client: Res<SteamClient>) {
-    steam_client.client.run_callbacks();
-    let matchmaking = steam_client.client.matchmaking();
-    let tx = steam_client.channel.sender.clone();
-
-    if keys.just_pressed(KeyCode::KeyC) {
-        matchmaking.create_lobby(LobbyType::FriendsOnly, 4, move |result| match result {
-            Ok(lobby_id) => {
-                tx.send(ChannelMessage::LobbyCreated(lobby_id)).unwrap();
-                println!("Created lobby: [{}]", lobby_id.raw())
-            }
-            Err(err) => panic!("Error: {}", err),
-        });
-    }
-
-    if keys.just_pressed(KeyCode::KeyT) {
-        let _ =
-            steam_client.send_message_others(NetworkData { data: vec![42] }, SendFlags::RELIABLE);
-    }
-
-    if keys.just_pressed(KeyCode::KeyJ) {
-        for friend in steam_client.client.friends().get_friends(FriendFlags::ALL) {
-            if let Some(game) = friend.game_played() {
-                if game.game.app_id() == AppId(480) {
-                    steam_client.join_lobby(game.lobby);
-                    println!("Auto join");
-                }
-            }
-        }
+        next_frame().await;
     }
 }
